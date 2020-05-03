7c6f76a80fdbf00b3cc7f8f4805f3e924fcd4f28
xpogit <azuredevopsmun@gmail.com>
2020-05-02 19:21:05 -0500

    two

--- src/test/java/papka/yoyo.java
+++ src/test/java/papka/yoyo.java
@@ -5,6 +5,8 @@ public class yoyo {
 
     public static void main(String[] args) {
         System.out.println("Run it for me!!!!!!");
+
+        System.out.println("Below mistake");
         s;
     }
 }
